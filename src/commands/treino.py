55c444e23c357ba2d52b174d438d5463e29e7163
dev-knz <knzfpsoficial@gmail.com>
2026-07-28 15:35:05 -0400

    Testing a view model with discord py

--- src/commands/treino.py
+++ src/commands/treino.py
@@ -3,6 +3,7 @@ from discord import app_commands
 from discord.ext import commands
 from random import randint
 from services.usuario import get_or_create_user
+from math_engine.arithmetic import add
 
 class Treino(commands.Cog):
 
@@ -29,15 +30,49 @@ class Treino(commands.Cog):
                 timeout = 30
             )
 
-            if int(answer.content) == sum(question):
+            if int(answer.content) == add(question):
                 await ctx.send('Acertou!')
 
             else:
-                await ctx.send(f'Errou! A resposta era {sum(question)}')
+                await ctx.send(f'Errou! A resposta era {add(question)}')
         except TimeoutError:
             await ctx.send('Você demorou para responder')
         except ValueError:
             await ctx.send('Digite apenas números')
+
+    @commands.command()
+    async def enviar_botao(self, ctx):
+        async def oie(interaction: discord.Interaction):
+            await interaction.response.send_message('Botão pressionado')
+
+        view = discord.ui.View()
+        button = discord.ui.Button(label='Botão', style=discord.ButtonStyle.red)    
+        button.callback = oie
+
+        view.add_item(button)
+
+        await ctx.reply(view=view)
+
+    @commands.command()
+    async def enviar_button(self, ctx):
+        async def select_resposta(interact: discord.Interaction):
+            escolha = interact.data['values'][0]
+            jogos = {'1': 'Free Fire', '2': 'Minecraft', '3':'Roblox'}
+            jogo_escolhido = jogos[escolha]
+            await interact.response.send_message(f'O jogo escolhido foi: {jogo_escolhido}')
+
+        menuSelecao = discord.ui.Select(placeholder='Selecione uma opção')
+        opcoes = [
+            discord.SelectOption(label='Free Fire', value='1'),
+            discord.SelectOption(label='Minecraft', value='2'),
+            discord.SelectOption(label='Roblox', value='3')
+        ]
+        menuSelecao.options = opcoes
+        menuSelecao.callback = select_resposta
+        view = discord.ui.View()
+        view.add_item(menuSelecao)
+
+        await ctx.reply(view=view)
 
 
 async def setup(bot):
